056de9b3c395b206b0c3e816d8c2ef2090a3b1e8
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-06-23 15:56:02 -0300

    first commit

---
+++ snapCrackle.js
@@ -0,0 +1,23 @@
+let mensagem = ''
+
+function snapCrackle (maxValue){
+  
+    for (let i = 1; i < maxValue; i++){
+        if(i % 5 == 0 && i % 2 !== 0 ) {
+            mensagem = mensagem + 'SnapCrackle'
+        }
+        else if (i % 2 !== 0){
+            mensagem = mensagem + 'Snap'
+        }
+        else if(i % 5 == 0) {
+            mensagem = mensagem + 'Crackle'
+        }
+        else if(i % 2 == 0 && i % 5 !== 0) {
+            mensagem = mensagem + i + ''
+        } 
+            mensagem = mensagem + ' , '
+    }
+    return mensagem
+}
+snapCrackle(12)
+console.log(snapCrackle(12))
